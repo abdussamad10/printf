9b166a47b1a90beaaf1cc79ffb8a38a6b15565f4
abdussamad10 <abdessamadseffir@gmail.com>
2023-04-24 05:09:19 -0700

    prin

--- _printf.c
+++ _printf.c
@@ -6,21 +6,49 @@
 /**
  * _printf - print formatted string
  * @format: the formatted string
- *
  * Return: the string length
  */
 
 int _printf(const char *format, ...)
 {
-	int i = 0;
-	va_list	args;
-
-	if (format == NULL)
-		return (-1);
-
+	int a;
+	int lenght, count = 0;
+	va_list args;
 	va_start(args, format);
 
-	i = _print_format(format, args);
+	lenght = strlen(format);
+
+	for (a = 0; a < lenght; a++)
+	{
+		if (*(format + a) == '%')
+		{
+			switch (*(format + a + 1))
+			{
+
+				case '%':
+					_putchar(va_arg(args, int));
+					count++;
+					break;
+				case 'c':
+					_putchar(va_arg(args, int));
+					count++;
+					break;
+				case 's':
+					_printer(va_arg(args, char *));
+					count++;
+					break;
+				default:
+					break;
+			}
+		}
+		else
+		{
+			_putchar(*(format + a));
+			count++;
+		}
+	}
+
 	va_end(args);
-	return (i);
+
+	return (count);
 }
