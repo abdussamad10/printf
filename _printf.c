78a2f99da029beb8493bdf164fba91f9f4b1eadd
abdellatif <buss502179@gmail.com>
2023-04-24 13:08:06 -0700

    ellah

--- _printf.c
+++ _printf.c
@@ -3,6 +3,7 @@
 #include <stdarg.h>
 #include <string.h>
 
+void print_buffer(char bufer[], int *buff_ind);
 /**
  * _printf - print formatted string
  * @format: the formatted string
@@ -12,15 +13,57 @@
 
 int _printf(const char *format, ...)
 {
-	int i = 0;
-	va_list	args;
+	int i, printed_chars = 0;
+	int flags, width, precision, size , buff_ind =0;
+
+	va_list list;
+	char buffer[BUUF_SIZE];
 
 	if (format == NULL)
 		return (-1);
 
-	va_start(args, format);
+	va_start(list, format);
 
-	i = _print_format(format, args);
-	va_end(args);
-	return (i);
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_in);
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer;
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
+		print_buffer(buffer, &buff_ind);
+
+		va_end(list);
+
+		return (printed_chars);
 }
+
+/*
+ * printed_buffer - prints the contents of the buffer if it exist
+ * @buffer: array of chars
+ * @buff_ind: index at which to add next char, represents the lenght.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if(*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+	*buff_ind = 0;
+}
+		
--- main.h
+++ main.h
@@ -1,9 +1,21 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdarg.h>
 
-#include <unistd.h>
+#include <stdio.h>
 #include <stdlib.h>
-#include <limits.h>
+#include <unistd.h>
+
+#define UNUSED(x) (void)(x)
+#define BUFF_SIZE 1024
+
+/*flags*/
+#define F_MINUX 1
+#define F_PLUS 2
+#define F_ZERO 4
+#define F_HASH 8
+#define F_SPACE 16
+
 int _putchar(char c);
 int _puts(char *str);
 void print_num(long n, int *len);
