b2b7a934ebfc7803d6a18206b9876b532e7b4f01
abdellatif <buss502179@gmail.com>
2023-04-25 10:54:49 -0700

    all

--- main.h
+++ main.h
@@ -40,7 +40,7 @@ struct fmt
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *i,
+int handle_print(const char *fmt, int *ind,
 va_list list, char buffer[], int flags, int width, int precision, int size);
 
 /****************** FUNCTIONS ******************/
