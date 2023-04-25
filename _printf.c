9e775ec8f9d8e7e6472b97f0589e525cca9edaa3
abdellatif <buss502179@gmail.com>
2023-04-25 05:52:08 -0700

    aa

--- _printf.c
+++ _printf.c
@@ -63,9 +63,5 @@ void print_buffer(char buffer[], int *buff_ind)
 		write(1, &buffer[0], *buff_ind);
 
 	*buff_ind = 0;
-<<<<<<< HEAD
-}	
-=======
 }
 
->>>>>>> c2fc794494c3193016a34eb453ae752d4b3737c4
