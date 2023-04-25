0a2cfd18a963f635d1c8be3c082aafbc81d7ee67
abdussamad10 <abdessamadseffir@gmail.com>
2023-04-25 12:43:13 -0700

    fonc

--- main.h
+++ main.h
@@ -40,8 +40,8 @@ struct fmt
 typedef struct fmt fmt_t;
 
 int _printf(const char *format, ...);
-int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
-        int flags, int width, int precision, int size);
+int handle_print(const char *fmt, int *i,
+va_list list, char buffer[], int flags, int width, int precision, int size);
 
 /****************** FUNCTIONS ******************/
 
@@ -115,3 +115,4 @@ long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
 #endif /* MAIN_H */
+
